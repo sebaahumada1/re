00298407c541612610340809fb1762c607a83730
sebaahumada1 <84730122+sebaahumada1@users.noreply.github.com>
2022-01-25 12:13:39 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,35 @@
+
+let botonaso= document.getElementById('botonaso')
+
+       botonaso.addEventListener("click",luz,true)
+       uno=0
+
+ function luz(){
+
+       if(uno==0){
+
+              let body=document.getElementById("body")
+              body.style.background="black"
+              body.style.color="white"
+              uno+=1
+                     
+              let so=document.getElementById("so")
+              so.style.color="black"
+              let sol=document.getElementById("sol")
+              sol.style.color="black"
+              let sole=document.getElementById("sole")
+              sole.style.color="white"
+
+            
+       }else{
+              body{
+					width: 100%;
+					height: 100vh;
+					background-color: rgba(0,0,0,0);
+					overflow: scroll;
+						uno=0
+
+             		 
+       }
+
+}
